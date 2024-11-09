ae5f970ba2e1ed5a7479eb3e567622af0e18f54a
Kasra Bigdeli <kasra85@gmail.com>
2024-11-09 10:31:58 -0800

    updated to Ubuntu 22 and reformat

--- scripts/01-docker.sh
+++ scripts/01-docker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
 # From: https://github.com/digitalocean/droplet-1-clicks/tree/master/common/scripts
 
 # DOCKER
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-cat > /etc/apt/sources.list.d/docker.list <<EOM
+cat >/etc/apt/sources.list.d/docker.list <<EOM
 deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -c -s) stable
 EOM
 
@@ -16,11 +15,9 @@ apt-get -y install docker-ce
 systemctl enable docker
 systemctl start docker
 
-
 # DOCKER COMPOSE
-sudo curl -L "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
-chmod +x /usr/local/bin/docker-compose;
-
+sudo curl -L "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # GRUB OPTS
 sed -e 's|GRUB_CMDLINE_LINUX="|GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1|g' \
@@ -28,24 +25,19 @@ sed -e 's|GRUB_CMDLINE_LINUX="|GRUB_CMDLINE_LINUX="cgroup_enable=memory swapacco
 
 update-grub
 
-
 # DOCKER DNS
 sed -e 's|#DOCKER_OPTS|DOCKER_OPTS|g' \
     -i /etc/default/docker
 
-
-
 #UFW
 
 ufw limit ssh
-ufw allow 80,443,3000,996,7946,4789,2377/tcp; ufw allow 7946,4789,2377/udp;
+ufw allow 80,443,3000,996,7946,4789,2377/tcp
+ufw allow 7946,4789,2377/udp
 ufw --force enable
 
-
 ## cleanup do-agent: https://github.com/digitalocean/marketplace-partners/issues/166
-apt-get purge droplet-agent* -y ||  echo "No DO Agent Found"
-
-
+apt-get purge droplet-agent* -y || echo "No DO Agent Found"
 
 ### CapRover
 docker pull caprover/caprover:VAR_CAPROVER_VERSION
--- scripts/90-cleanup.sh
+++ scripts/90-cleanup.sh
@@ -27,7 +27,7 @@ fi
 
 rm -rf /tmp/* /var/tmp/*
 history -c
-cat /dev/null > /root/.bash_history
+cat /dev/null >/root/.bash_history
 unset HISTFILE
 find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
 rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
@@ -47,4 +47,5 @@ The secure erase will complete successfully when you see:${NC}
 Beginning secure erase now\n"
 
 dd if=/dev/zero of=/zerofile bs=4096 || rm /zerofile
-cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
+cat /dev/null >/var/log/lastlog
+cat /dev/null >/var/log/wtmp
